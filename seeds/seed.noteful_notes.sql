454637ece8b9a3871150fd08a15ae7ed894e68f5
Timothy Hanke <tim.hanke@gmail.com>
2020-11-17 20:19:40 -0500

    added migrations, seeds, folders services, notes services, and folders router

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_notes (name, content, date_modified, folder_id)
+VALUES
+  ('Dogs', 'Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui.', '2019-01-03T00:00:00.000Z', '1'),
+  ('Cats', 'Eos laudantium quia ab blanditiis temporibus necessitatibus. Culpa et voluptas ut sed commodi.', '2018-08-15T23:00:00.000Z', '2'),
+  ('Pigs', 'Occaecati dignissimos quam qui facere deserunt quia. Quaerat aut eos laudantium dolor odio officiis illum.', '2018-03-01T00:00:00.000Z', '3');
